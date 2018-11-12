347aab42e43685c5b9de3438c2bc43b49d0799ff
Juan Biscaia <juan@hariken.co>
2018-11-12 17:47:32 -0200

    Writing some tests

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,23 +1,47 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 
-import SearchBar from 'src/components/SearchBar/SearchBar';
+import thunk from 'redux-thunk';
+import reducers from 'src/reducers/combine';
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+
 import Header from 'src/components/Header/Header';
-
+import SearchBar from 'src/components/SearchBar/SearchBar';
 import VenuesList from 'src/components/Venues/VenuesList';
+import { fetchVenues, clearVenues } from 'src/actions/Actions';
+
 import 'node_modules/bootstrap/dist/css/bootstrap.min.css';
 
+const store = createStore(reducers, applyMiddleware(thunk));
+
 class App extends Component {
+
+  componentDidMount() {
+    if ('geolocation' in navigator) {
+      navigator.geolocation.getCurrentPosition(position => {
+          const { latitude, longitude } = position.coords;
+          store.dispatch(fetchVenues({ ll: `${latitude}, ${longitude}` }));
+        }, () =>
+          store.dispatch(clearVenues())
+      );
+    } else {
+      store.dispatch(clearVenues());
+    }
+  }
+
   render() {
     return (
-      <div className="container">
-        <div className="row" style={stylesApp}>
-          <div className="col-md">
-            <Header/>
-            <SearchBar/>
-            <VenuesList/>
+      <Provider store={store}>
+        <div className="container">
+          <div className="row" style={stylesApp}>
+            <div className="col-md">
+              <Header/>
+              <SearchBar/>
+              <VenuesList/>
+            </div>
           </div>
         </div>
-      </div>
+      </Provider>
     );
   }
 }
--- src/index.js
+++ src/index.js
@@ -1,35 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createStore, applyMiddleware } from 'redux';
-import thunk from 'redux-thunk';
-import { Provider } from 'react-redux';
 import * as serviceWorker from './serviceWorker';
-
 import App from './components/App/App';
-import reducers from 'src/reducers/combine';
-import { fetchVenues, clearVenues } from './actions/Actions';
 
-const store = createStore(reducers, applyMiddleware(thunk));
-
-// This is ugly... Calling actions inside the main index.js...
-// But, since I have a limited time to make decisions, the initial call was made here.
-if ('geolocation' in navigator) {
-  navigator.geolocation.getCurrentPosition(position => {
-      const { latitude, longitude } = position.coords;
-      store.dispatch(fetchVenues({ ll: `${latitude}, ${longitude}` }));
-    }, () =>
-      store.dispatch(clearVenues())
-  );
-} else {
-  store.dispatch(clearVenues());
-}
-
-ReactDOM.render(
-  <Provider store={store}>
-    <App/>
-  </Provider>,
-  document.getElementById('root')
-);
+ReactDOM.render(<App/>, document.getElementById('root'));
 
 
 // If you want your app to work offline and load faster, you can change
